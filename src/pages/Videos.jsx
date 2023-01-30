09c7325e2f446ad006bf68c13381fff667a37f31
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2023-01-30 20:15:18 +0900

    apply axios

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
   const { isLoading, error, data: videos} = useQuery(
     ['videos', keyword],
-    async () => { return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`).then(res => res.json()).then(data => data.items)}
+    async () => { return axios.get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+    .then(res => res.data.items)}
   );
   return (
     <>
