cce3e771245af01a239cd08d95a6d98a7dfeecdd
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2023-01-31 17:52:24 +0900

    implement video card & apply timeago.js as util_e

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { formatAgo } from '../util/date';
 
 export default function VideoCard({ video }) {
   const { id, snippet } = video;
   const { title, thumbnails, channelTitle, publishedAt } = snippet;
-  console.log(thumbnails);
   return (
     <li key={id} className='flex flex-col max-w-xs p-1'>
-      <img src={thumbnails.high.url} alt="thumnails" className=''/>
-      <span>{title}</span>
-      <span>{channelTitle}</span>
-      <span>{publishedAt}</span>
+      <img src={thumbnails.high.url} alt={title} className=''/>
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt)}</p>
+      </div>
     </li>
   );
 }
