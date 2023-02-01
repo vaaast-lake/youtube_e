87af770f18cd66d26c1dc35742ba7dcbcaee8700
Kwon Dae-ho <101388919+vaaast-lake@users.noreply.github.com>
2023-02-01 15:32:51 +0900

    bugfix: invalid API_KEY

--- src/api/youtubeClient.js
+++ src/api/youtubeClient.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default class YoutubeClient {
   constructor() {
     this.httpClient = axios.create({
-      baseURL: 'https://www.googleapis.com/youtube/v3',
-      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY}
+      baseURL: 'https://www.googleapis.com/youtube/v3/',
+      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY }
     });
   }
 
